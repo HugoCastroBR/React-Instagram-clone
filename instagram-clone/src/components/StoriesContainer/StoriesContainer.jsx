766a6ec00a3e806c49be875c524f41410d81e999
Hugay <61667328+Hugay@users.noreply.github.com>
2021-03-21 17:41:06 -0300

    stories button

--- instagram-clone/src/components/StoriesContainer/StoriesContainer.jsx
+++ instagram-clone/src/components/StoriesContainer/StoriesContainer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled  from 'styled-components';
+import {default as NextStorieSVG} from '../../static/img/NextStorie.svg'
 
 function StoriesContainer({children}){
     const StoriesContainer = styled.div`
         height: 118px;
-        background-color: red;
+
         display: flex;
         align-items: center;
     `
@@ -12,19 +13,29 @@ function StoriesContainer({children}){
     const StoriesCarouselContainer = styled.div`
         height: 84px;
         min-width: 100%;
-        background-color: blue;
     `
 
     const CarouselControlButton = styled.button`
         position: absolute;
         margin-left: ${props =>`${props.margin?props.margin:0}px`};
-        width: 25px;
-        height: 25px;
+        width: 22px;
+        height: 22px;
+        border-radius: 50px;
+        overflow:hidden;
         cursor: pointer;
+        background-image: url(${NextStorieSVG});
+        background-color: transparent;
+        border: 0px solid white;
+
+        box-shadow: inset 0px 0px 0px 2px white;
+        box-shadow:  0px 0px 6px 0.1px #b6b6b6;
+
+        transform: rotate(${props =>`${props.rotate?props.rotate:0}deg`});
+
     `
     return(
         <StoriesContainer>
-            <CarouselControlButton margin={12}/>
+            <CarouselControlButton margin={12} rotate={180}/>
                 <StoriesCarouselContainer>
 
                 </StoriesCarouselContainer>
