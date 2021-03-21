07e613d741ef7753fb2c768f71c8fec55bc7ea3b
Hugay <61667328+Hugay@users.noreply.github.com>
2021-03-20 21:27:14 -0300

    post container component

--- instagram-clone/src/components/page/page.jsx
+++ instagram-clone/src/components/page/page.jsx
@@ -17,16 +17,36 @@ function Page({children,theme}){
     const SectionContainer = styled.div`
         background-color: ${theme.secondaryColor};
         width: 100vw;
-        height: 100vh;
+        min-height: 100vh;
         max-width: 935px;
+        overflow-x: hidden;
+        display: flex;
+        flex-wrap: no-wrap;
     `;
 
-    
+    const MainContentContainer = styled.div`
+        width: ${props => `${props.size[0]?props.size[0]:614}px`};
+        max-width: 614px;
+        overflow-x: hidden;
+        background-color: ${theme.mainColor};
+        border: 1px solid #e1e1e1;
+        border-radius: 3px;
+        min-height: 10px;
+        height: ${props => `${props.size[1]}px`};
+        margin-top: 24px;
+
+    `
+    const StoriesContainer = styled.div`
+        height: 60px;
+    `
+
     return(
         <PageContainer>
             <Header/>
             <SectionContainer>
-                {children}
+                <MainContentContainer size={[null, 60]}>
+                    <StoriesContainer/>
+                </MainContentContainer>
             </SectionContainer>
         </PageContainer>
     )
