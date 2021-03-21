3245321df29d61cda763c2564274c7ba334fa513
Hugay <61667328+Hugay@users.noreply.github.com>
2021-03-21 18:00:29 -0300

    stories body

--- instagram-clone/src/components/StoriesContainer/StoriesContainer.jsx
+++ instagram-clone/src/components/StoriesContainer/StoriesContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled  from 'styled-components';
-import {default as NextStorieSVG} from '../../static/img/NextStorie.svg'
+import {default as NextStorySVG} from '../../static/img/NextStorie.svg'
 
 function StoriesContainer({children}){
     const StoriesContainer = styled.div`
@@ -8,6 +8,7 @@ function StoriesContainer({children}){
 
         display: flex;
         align-items: center;
+
     `
 
     const StoriesCarouselContainer = styled.div`
@@ -23,21 +24,63 @@ function StoriesContainer({children}){
         border-radius: 50px;
         overflow:hidden;
         cursor: pointer;
-        background-image: url(${NextStorieSVG});
+        background-image: url(${NextStorySVG});
         background-color: transparent;
         border: 0px solid white;
-
-        box-shadow: inset 0px 0px 0px 2px white;
-        box-shadow:  0px 0px 6px 0.1px #b6b6b6;
-
+        box-shadow: 0px 0px 5px 0.1px #b6b6b6,
+                    0px 0px 1px 1px white,
+                    inset 0px 0px 0px 2px white;
         transform: rotate(${props =>`${props.rotate?props.rotate:0}deg`});
 
+    `
+
+    const StoryItem = styled.div`
+        width: 72px;
+        height: 84px;
+        margin-left: 12px;
+        margin-right: 12px;
+        display: flex;
+        flex-direction: column;
+        justify-content:center;
+        align-items: center;
+
+        h3{
+            display: block;
+            height: 6px;
+            font-size: 10px;
+            margin-top: 4px;
+        }
+    `
+
+    const StoryItemImgContainer = styled.div`
+        margin-top: 2px;
+        width: 55px;
+        height: 55px;
+        background-color: transparent;
+        border: 2px solid red;
+        border-radius: 50px;
+        display: flex;
+        flex-direction: column;
+        justify-content:center;
+        align-items: center;
+        img{
+            border-radius: 50px;
+            width:52px;
+            height: 52px;
+            background-color: blue;
+            
+        }
     `
     return(
         <StoriesContainer>
             <CarouselControlButton margin={12} rotate={180}/>
                 <StoriesCarouselContainer>
-
+                    <StoryItem>
+                        <StoryItemImgContainer>
+                            <img src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt=""/>
+                        </StoryItemImgContainer>
+                        <h3>Teste do teste</h3>
+                    </StoryItem>
                 </StoriesCarouselContainer>
             <CarouselControlButton  margin={577}/>
         </StoriesContainer>
