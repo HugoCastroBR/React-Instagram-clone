13af06765e5795aff5b353edc62dd3d0f9fa16b5
Hugay <61667328+Hugay@users.noreply.github.com>
2021-03-25 15:09:00 -0300

    storie item component

--- instagram-clone/src/components/StoriesContainer/StoriesContainer.jsx
+++ instagram-clone/src/components/StoriesContainer/StoriesContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled  from 'styled-components';
 import {default as NextStorySVG} from '../../static/img/NextStorie.svg'
+import StorieItem from './../storieItem/index';
 
 function StoriesContainer({children}){
     const StoriesContainer = styled.div`
@@ -14,6 +15,9 @@ function StoriesContainer({children}){
     const StoriesCarouselContainer = styled.div`
         height: 84px;
         min-width: 100%;
+        display: flex;
+        padding-left: 12px;
+        padding-right: 12px;
     `
 
     const CarouselControlButton = styled.button`
@@ -32,55 +36,23 @@ function StoriesContainer({children}){
                     inset 0px 0px 0px 2px white;
         transform: rotate(${props =>`${props.rotate?props.rotate:0}deg`});
 
+
     `
 
-    const StoryItem = styled.div`
-        width: 72px;
-        height: 84px;
-        margin-left: 12px;
-        margin-right: 12px;
-        display: flex;
-        flex-direction: column;
-        justify-content:center;
-        align-items: center;
-
-        h3{
-            display: block;
-            height: 6px;
-            font-size: 10px;
-            margin-top: 4px;
-        }
-    `
-
-    const StoryItemImgContainer = styled.div`
-        margin-top: 2px;
-        width: 55px;
-        height: 55px;
-        background-color: transparent;
-        border: 2px solid red;
-        border-radius: 50px;
-        display: flex;
-        flex-direction: column;
-        justify-content:center;
-        align-items: center;
-        img{
-            border-radius: 50px;
-            width:52px;
-            height: 52px;
-            background-color: blue;
-            
-        }
-    `
+    
     return(
         <StoriesContainer>
             <CarouselControlButton margin={12} rotate={180}/>
                 <StoriesCarouselContainer>
-                    <StoryItem>
-                        <StoryItemImgContainer>
-                            <img src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt=""/>
-                        </StoryItemImgContainer>
-                        <h3>Teste do teste</h3>
-                    </StoryItem>
+                    {/* Espaço para 7 Stories */}
+                    <StorieItem/>
+                    <StorieItem/>
+                    <StorieItem/>
+                    <StorieItem/>
+                    <StorieItem/>
+                    <StorieItem/>
+                    <StorieItem/>
+                    
                 </StoriesCarouselContainer>
             <CarouselControlButton  margin={577}/>
         </StoriesContainer>
---
+++ instagram-clone/src/components/storieItem/StorieItem.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled  from 'styled-components';
+
+function StorieItem(){
+    const StoryItem = styled.div`
+        width: 72px;
+        height: 84px;
+        margin-left: 5px;
+        margin-right: 5px;
+        display: flex;
+        flex-direction: column;
+        justify-content:center;
+        align-items: center;
+
+        h3{
+            display: block;
+            height: 6px;
+            font-size: 10px;
+            margin-top: 4px;
+        }
+    `;
+
+    const StoryItemImgContainer = styled.div`
+        margin-top: 2px;
+        width: 55px;
+        height: 55px;
+        background-color: transparent;
+        border: 2px solid red;
+        border-radius: 50px;
+        display: flex;
+        flex-direction: column;
+        justify-content:center;
+        align-items: center;
+        img{
+            border-radius: 50px;
+            width:52px;
+            height: 52px;
+            background-color: blue;
+            
+        }
+    `;
+
+    return(
+        <StoryItem>
+            <StoryItemImgContainer>
+                <img
+                    src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
+                    alt=""
+                />
+            </StoryItemImgContainer>
+            <h3>Teste do teste</h3>
+        </StoryItem>
+    )
+}
+
+export default StorieItem;
