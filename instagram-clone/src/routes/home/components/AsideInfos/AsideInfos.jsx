f2c8dfce775a9b6973ad8c7dfc6875a4699db2ce
Hugay <61667328+Hugay@users.noreply.github.com>
2021-04-04 11:30:48 -0300

    Feat: Follow suggestion

--- instagram-clone/src/routes/home/components/AsideInfos/AsideInfos.jsx
+++ instagram-clone/src/routes/home/components/AsideInfos/AsideInfos.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import UserImage from './../../../../components/UserImage/UserImage';
-
-
+import UserImage from "./../../../../components/UserImage/UserImage";
 
 const AsideInfosContainer = styled.div`
-    display:flex;
-    flex-direction:column;
+    display: flex;
+    flex-direction: column;
     align-items: flex-start;
     justify-content: center;
-
-`
+`;
 
 const YourUserInfos = styled.div`
     margin-top: 6px;
@@ -20,53 +17,44 @@ const YourUserInfos = styled.div`
     justify-content: space-between;
     height: 56px;
 
-    & a{
+    & a {
         margin-top: 20px;
         text-decoration: none;
         color: #0095f6;
         font-size: 12px;
         margin-right: 16px;
     }
-
-`
+`;
 const MainInfos = styled.div`
     width: 245px;
     height: 56px;
     display: flex;
     align-items: center;
-    & div:first-child{
+    & div:first-child {
         width: 56px;
     }
-    & div:last-child{
+    & div:last-child {
         margin-left: 6px;
         display: flex;
         flex-direction: column;
     }
-    
-`
+`;
 
 const SuggestedPeoples = styled.div`
     display: flex;
     flex-direction: column;
-    width: 100%;
-    & :first-child{
+    width: 295px;
+    
+`;
+
+const SeeAllSuggestContainer = styled.div`
         margin-top: 10px;
         width: 100%;
         display: flex;
         justify-content: space-between;
         font-size: 13px;
-        & a{
-            
-            display: flex;
-            width: 100px;
-            text-decoration: none;
-            color: black;
-            justify-content: flex-end;
-            margin-left: 30px;
-            
-        }
-    }
 
+    
 `
 
 const SuggestedPeoplesList = styled.div`
@@ -74,75 +62,119 @@ const SuggestedPeoplesList = styled.div`
     display: flex;
     flex-direction: column;
     height: auto;
-    width: 100%;
-`
+    width: 295px;
+`;
 
 const SuggestedPeopleStyle = styled.div`
     display: flex;
     height: 40px;
-    width: 100%;
-    background-color: red;
-`
+    width: 295px;
+
+    align-items: center;
+    & a {
+        margin-top: 13px;
+        height: 32px;
+        display: flex;
+        flex-direction: column;
+        color: #0095f6;
+        font-size: 13px;
+        text-decoration: none;
+    }
+`;
 
 const SuggestedMainInfos = styled.div`
     width: 245px;
     height: 40px;
     display: flex;
-    align-items: center;
-    & div:first-child{
-        width: 32px;
+    align-items: flex-end;
+    justify-content: flex-start;
+    & div:first-child {
+        width: 34px;
+        margin: 0px;
+        height: 34px;
     }
-    & a:last-child{
-        margin-left: 6px;
+    & div:last-child {
+        display: block;
+        height: 34px;
+        margin: 0px;
+
         display: flex;
         flex-direction: column;
-        background-color: #0095f6;
+        justify-content: center;
+        font-size: 12px;
+        margin-left: 8px;
+        width: 200px;
+        overflow-x: hidden;
+        overflow-y: hidden;
     }
-    `
+`;
 
-function SuggestedPeople(){
-    return(
+const SeeAllSuggest = styled.a`
+    display: flex;
+    width: 100px;
+    text-decoration: none;
+    color: black;
+    justify-content: flex-end;
+    margin-right: 4px;
+`;
+
+function SuggestedPeople() {
+    return (
         <SuggestedPeopleStyle>
             <SuggestedMainInfos>
-                    <div>
-                        <UserImage size='32' src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt=""/>
-                    </div>
-                    <div>
-                        <span><b>Hugo Castro</b></span>
-                        <span>Hugo</span>
-                    </div>
-                </SuggestedMainInfos>
-                    <a href="/">Seguir</a>
+                <div>
+                    <UserImage
+                        size="32"
+                        src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <span>
+                        <b>Hugo Castro</b>
+                    </span>
+                    <span>Suggested to you</span>
+                </div>
+            </SuggestedMainInfos>
+            <a href="/">Seguir</a>
         </SuggestedPeopleStyle>
-    )
+    );
 }
 
-function AsideInfos(){
-    return(
+function AsideInfos() {
+    return (
         <AsideInfosContainer>
             <YourUserInfos>
                 <MainInfos>
                     <div>
-                        <UserImage size='56' src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt=""/>
+                        <UserImage
+                            size="56"
+                            src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
+                            alt=""
+                        />
                     </div>
                     <div>
-                        <span><b>Hugo Castro</b></span>
+                        <span>
+                            <b>Hugo Castro</b>
+                        </span>
                         <span>Hugo</span>
                     </div>
                 </MainInfos>
-                    <a href="/">Mudar</a>
+                <a href="/">Mudar</a>
             </YourUserInfos>
             <SuggestedPeoples>
-                <div>
+                <SeeAllSuggestContainer>
                     <span>Sugestões para você</span>
-                    <a href="/"><b>Ver tudo</b></a>
-                </div>
+                    <SeeAllSuggest href="/">
+                        <b>Ver tudo</b>
+                    </SeeAllSuggest>
+                </SeeAllSuggestContainer>
                 <SuggestedPeoplesList>
-                    <SuggestedPeople/>
+                    <SuggestedPeople />
                 </SuggestedPeoplesList>
             </SuggestedPeoples>
         </AsideInfosContainer>
-    )
+    );
 }
 
 export default AsideInfos;
